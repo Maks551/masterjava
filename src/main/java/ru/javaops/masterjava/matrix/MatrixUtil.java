7218c5a1f0d7bf597d5e724a125e5e4556562b8a
Maxim Kurkudyk <Maksik551@gmail.com>
2018-12-10 17:53:26 +0200

    HW_0

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,21 +1,54 @@
 package ru.javaops.masterjava.matrix;
 
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * gkislin
  * 03.07.2016
  */
 public class MatrixUtil {
+    private static final Lock LOCK = new ReentrantLock();
+    private static byte COUNT = 0;
+    private static final byte THREADS_COUNT = 10;
 
     // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+
+        for (int i = 0; i < matrixSize; i = i + matrixSize/ THREADS_COUNT) {
+            executor.submit(getTaskR(matrixSize, i, matrixA, matrixB, matrixC));
+        }
+        while (COUNT != THREADS_COUNT) {
+            Thread.sleep(100);
+        }
+        COUNT = 0;
+
         return matrixC;
+    }
+
+    private static Runnable getTaskR(int matrixSize, int n, int[][] matrixA, int[][] matrixB, int[][] matrixC) {
+        return () -> {
+            int size = matrixSize / THREADS_COUNT + n;
+            for (int i = n; i < size; i++) {
+                try {
+                    for (int j = 0; ; j++) {
+                        int sum = 0;
+                        for (int k = 0; k < matrixSize; k++) {
+                            sum += matrixA[i][k] * matrixB[k][j];
+                        }
+                        matrixC[i][j] = sum;
+                    }
+                } catch (IndexOutOfBoundsException ignored) { }
+            }
+            LOCK.lock();
+            COUNT++;
+            LOCK.unlock();
+        };
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
